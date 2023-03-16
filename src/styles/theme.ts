34e8ea906d897e9ee8b49d341da54a9bc2f5e6f4
dongsu <sonicce99@naver.com>
2023-03-16 19:38:00 +0900

    feat : styled-components theme 생성

---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  colors: {
+    primary: "#3396ff",
+    white: "#ffffff",
+    black: "#000000",
+  },
+};
