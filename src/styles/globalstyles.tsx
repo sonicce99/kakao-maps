147ae3ebf5f490b88216ff46f7ee6682a5847d78
dongsu <sonicce99@naver.com>
2023-03-17 17:47:01 +0900

    docs : global style 생성

---
+++ src/styles/globalstyles.tsx
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  * {
+    box-sizing: border-box;
+  }
+
+  button {
+      border: none;
+  }
+`;
+
+export default GlobalStyle;
