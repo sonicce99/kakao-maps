0d4431c68553609a4c525591e510d6e92cdbebc2
dongsu <sonicce99@naver.com>
2023-03-17 17:54:15 +0900

    feat: color, url 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,5 +5,11 @@ export const theme: DefaultTheme = {
     primary: "#3396ff",
     white: "#ffffff",
     black: "#000000",
+    gray: "#888",
+    lightGray: "#e2e2e2",
+  },
+
+  url: {
+    sign: "https://t1.daumcdn.net/localimg/localimages/07/2018/pc/common/img_search.png",
   },
 };
